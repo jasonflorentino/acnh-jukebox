554b9fd5e70f4f74903649a52de196519f586ce4
Jason Florentino <jasonmflorentino@gmail.com>
2021-12-19 17:22:45 -0500

    Add custom loopback timer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
 import { useState, useRef, useEffect } from 'react';
-
 import Head from 'next/head';
+
 import SongArt from '@/components/SongArt';
 import Player from '@/components/Player';
 import Footer from '@/components/Footer';
 import TouchPrompt from '@/components/TouchPrompt';
 import useIsTouchDevice from '@/lib/hooks/useIsTouchDevice';
 import useLocalStorage from '@/lib/hooks/useLocalStorage';
-
+import restartTimes from '@/lib/restartTimes';
 import styles from '@/styles/Home.module.scss';
 
 export default function Home({ songs }: { songs: Song[] }) {
@@ -30,6 +30,71 @@ export default function Home({ songs }: { songs: Song[] }) {
       }
     }
   }, [isTouchDevice, requiresTouchPrompt, storage])
+
+  /**
+   * Handles starting a loop-back timer when
+   * the current song changes to provide
+   * more seamless looping of songs for which
+   * we know a restart time.
+   * 
+   * Implements a method I experimented with here:
+   * https://codepen.io/jasonflorentino/pen/mdBwJpd?editors=1010
+   */
+  useEffect(() => {
+    const { current: audioEl } = audioRef;
+    const restartTime = restartTimes[currentSong ? currentSong.id : '0'];
+    const TIMEOUT = 1;
+    const INTERVAL = 2;
+    let timerType: number;
+    let timerId: NodeJS.Timer | NodeJS.Timeout;
+
+    const cancelPoll = () => {
+      if (timerId) {
+        if (timerType === TIMEOUT) {
+          clearTimeout(timerId); 
+        } else if (timerType === INTERVAL) {
+          clearInterval(timerId);
+        }
+      }
+    }
+
+    const pollForRestart = (
+      audio: HTMLMediaElement, 
+      restartTime: number, // Seconds
+      step: number // Milliseconds
+    ) => {
+      // no op for unknown restart time
+      if (restartTime === -1) return setTimeout(() => {}, 100);
+      cancelPoll();
+
+      if (step <= 2000) {
+        timerType = INTERVAL
+        timerId = setInterval(() => {
+          if (audio.currentTime >= restartTime) {
+            audio.currentTime = 0;
+            clearInterval(timerId);
+            // Start long polling for next loop
+            timerId = pollForRestart(audio, restartTime, restartTime * 1000);
+            timerType = TIMEOUT;
+          }
+        }, 50);
+        return timerId;
+      }
+    
+      timerType = TIMEOUT;
+      return setTimeout(() => {
+        timerId = pollForRestart(audio, restartTime, step / 2);
+      }, step / 2);
+    }
+
+    if (audioEl && currentSong) {
+      timerId = pollForRestart(audioEl, restartTime, restartTime * 1000);
+    }
+
+    return () => {
+      cancelPoll();
+    }
+  }, [currentSong])
 
   /**
    * Function for exclicitly playing the
