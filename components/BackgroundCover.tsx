a2c3e75a3b04fd9d9bcfbcc48e246cee281a56ca
Jason Florentino <jasonmflorentino@gmail.com>
2022-02-06 21:40:02 -0500

    Play around with transition effect

--- components/BackgroundCover.tsx
+++ components/BackgroundCover.tsx
@@ -1,25 +1,25 @@
 import { useState } from 'react';
+
 import styles from '@/components/BackgroundCover.module.scss';
+import useTransition from '@/lib/hooks/useTransition';
 
-const BackgroundCover = ({onClick}: {onClick: () => void}) => {
-  const [exit, setExit] = useState(false);
-
-  const handleClick = () => {
-    setExit(true);
-    setTimeout(() => {
-      onClick();
-    }, 200)
-  }
+const BackgroundCover = ({ onClick }: { onClick: () => void }) => {
+  const { isEntering, isExiting, exit } = useTransition({
+    timingMs: 250,
+    onExit: onClick,
+  });
 
   return (
-    <div 
-    className={[
-        styles.BackgroundCover, 
-        exit ? styles.fadeOut : styles.fadeIn,
-      ].join(' ')} 
-      onClick={handleClick}
+    <div
+      className={[
+        styles.BackgroundCover,
+        isEntering ?  styles.fadeIn : '',
+        isExiting ?  styles.fadeOut : '',
+        !isEntering && ! isExiting ? styles.resting : '',
+      ].join(' ')}
+      onClick={exit}
     ></div>
-  )
-}
+  );
+};
 
 export default BackgroundCover;
---
+++ lib/hooks/useTransition.ts
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react';
+
+const noop = () => {};
+
+const useTransition = ({
+  timingMs = 250,
+  onExit = noop,
+}: {
+  timingMs: number;
+  onExit: () => void;
+}) => {
+  const [isEntering, setIsEntering] = useState(true);
+  const [isExiting, setIsExiting] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsEntering(false);
+    }, timingMs);
+  }, []);
+
+  const exit = () => {
+    setIsExiting(true);
+    setTimeout(() => {
+      onExit();
+    }, timingMs);
+  };
+
+  return {
+    isEntering,
+    isExiting,
+    exit,
+  };
+};
+
+export default useTransition;
